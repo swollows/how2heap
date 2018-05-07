b23ceb0d893efbfe771567e4e0783eb19af7ef0b
swollows <git.wngur9081@gmail.com>
2018-05-07 23:38:24 +0900

    Update overlapping_chunks_2.c

--- overlapping_chunks_2.c
+++ overlapping_chunks_2.c
@@ -48,15 +48,19 @@ int main(){
   memset(p4,'D',real_size_p4);
   memset(p5,'E',real_size_p5);
   
-  fprintf(stderr, "\nLet's free the chunk p4.\nIn this case this isn't coealesced with top chunk since we have p5 bordering top chunk after p4\n"); 
+  fprintf(stderr, "\nLet's free the chunk p4.\nIn this case this isn't coealesced with top chunk \
+  since we have p5 bordering top chunk after p4\n"); 
   
   free(p4);
 
-  fprintf(stderr, "\nLet's trigger the vulnerability on chunk p1 that overwrites the size of the in use chunk p2\nwith the size of chunk_p2 + size of chunk_p3\n");
+  fprintf(stderr, "\nLet's trigger the vulnerability on chunk p1 that overwrites the size of the in use chunk p2\
+  \nwith the size of chunk_p2 + size of chunk_p3\n");
 
-  *(unsigned int *)((unsigned char *)p1 + real_size_p1 ) = real_size_p2 + real_size_p3 + prev_in_use + sizeof(size_t) * 2; //<--- BUG HERE 
+  *(unsigned int *)((unsigned char *)p1 + real_size_p1 ) = real_size_p2 + real_size_p3 + prev_in_use + sizeof(size_t) * 2; 
+  //<--- BUG HERE 
 
-  fprintf(stderr, "\nNow during the free() operation on p2, the allocator is fooled to think that \nthe nextchunk is p4 ( since p2 + size_p2 now point to p4 ) \n");
+  fprintf(stderr, "\nNow during the free() operation on p2, the allocator is fooled to think that \
+  \nthe nextchunk is p4 ( since p2 + size_p2 now point to p4 ) \n");
   fprintf(stderr, "\nThis operation will basically create a big free chunk that wrongly includes p3\n");
   free(p2);
   
@@ -65,7 +69,8 @@ int main(){
   p6 = malloc(2000);
   real_size_p6 = malloc_usable_size(p6);
 
-  fprintf(stderr, "\nOur malloc() has been satisfied by our crafted big free chunk, now p6 and p3 are overlapping and \nwe can overwrite data in p3 by writing on chunk p6\n");
+  fprintf(stderr, "\nOur malloc() has been satisfied by our crafted big free chunk, now p6 and p3 are overlapping and \
+  \nwe can overwrite data in p3 by writing on chunk p6\n");
   fprintf(stderr, "\nchunk p6 from %p to %p", p6,  (unsigned char *)p6+real_size_p6);
   fprintf(stderr, "\nchunk p3 from %p to %p\n", p3, (unsigned char *) p3+real_size_p3); 
 
